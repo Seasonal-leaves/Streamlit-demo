0c702921b8aefbe9a471c02d7fb87c6ada566486
Seasonal-leaves <143732917+Seasonal-leaves@users.noreply.github.com>
2023-08-31 21:13:08 +0530

    Create streamlit_demo_app.py

---
+++ streamlit_demo_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+'''def find_largest(num1,num2,num3):
+  return max(num1,num2,num3)'''
+def main():
+  st.write("""
+  #Finding largest among 3 numbers app
+  """)
+  st.header("User input numbers")
+  n1=st.number_input("Enter your 1st number :")
+  n2=st.number_input("Enter your 2nd number :")
+  n3=st.number_input("Enter your 3rd number :")
+  if st.button("Which is the largest?"):
+    #largest = find_largest(n1,n2,n3)
+    st.success("The largest number is :" max(n1,n2,n3))
+if __name__=="__main__":
+  main()
+    
