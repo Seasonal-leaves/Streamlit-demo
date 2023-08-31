b970fad93c19f6995282dcac9779ddcb7ee62cea
Seasonal-leaves <143732917+Seasonal-leaves@users.noreply.github.com>
2023-08-31 21:34:54 +0530

    Update streamlit_demo_app.py

--- streamlit_demo_app.py
+++ streamlit_demo_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-#def find_largest(num1,num2,num3):
- # return max(num1,num2,num3)
+
 def main():
   st.title("Finding largest among 3 numbers app")
   st.header("User input numbers")
@@ -8,9 +7,8 @@ def main():
   n2=st.number_input("Enter your 2nd number :")
   n3=st.number_input("Enter your 3rd number :")
   if st.button("Which is the largest?"):
-    #largest = find_largest(n1,n2,n3)
-    max_num =  max(n1,n2,n3)
-    st.success(f"The largest number is : {max_num}")
+    max_num =  max(n1,n2,n3) 
+    st.success(f"The largest number is : {max_num}") #What I have found out from my prior changes is that streamlit needs formatted print statements rather that multiple arguments
 if __name__=="__main__":
   main()
     
